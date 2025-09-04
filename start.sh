47af25701e7fdea8e1c2c7fcb798b85e0e5c7e6c
kokomoja <supercluster16@gmail.com>
2025-09-04 16:43:54 +0700

    Make start.sh executable

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Starting app with gunicorn..."
-exec gunicorn app:app --bind 0.0.0.0:10000
+
+# ใช้ virtual environment ของ Render
+VENV_PATH=/opt/render/project/.venv
+
+# Activate venv (ทาง Render บังคับใช้ path ตรง)
+source $VENV_PATH/bin/activate
+
+# เริ่ม Flask app ด้วย Gunicorn
+$VENV_PATH/bin/gunicorn app:app --bind 0.0.0.0:10000
