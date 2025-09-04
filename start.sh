7ffdd0aa0912ceaf1915fff49f64fa2e6d3d031e
kokomoja <supercluster16@gmail.com>
2025-09-04 16:16:35 +0700

    fix: run gunicorn using python3 -m

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# start.sh
-# ใช้สำหรับรัน Flask บน Render ด้วย gunicorn
-
 echo "Starting app with gunicorn..."
-gunicorn app:app --bind 0.0.0.0:10000 --workers 4
+exec python3 -m gunicorn app:app --bind 0.0.0.0:10000
